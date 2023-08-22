7ea50a2f3006529b0c0498f70ef07ae3903d0b6f
dchristian09 <dchristian09@student.ciputra.ac.id>
2023-08-22 13:33:18 +0700

    FIX UI

--- Up/Scenes/GameOver/GameOver.swift
+++ Up/Scenes/GameOver/GameOver.swift
@@ -10,6 +10,8 @@ import SpriteKit
 class GameOver: SKScene {
     
     var starField: SKEmitterNode!
+    var weatherKitManager = WeatherKitManager()
+    var locationManager = LocationManager()
     var scoreNumberLabel: SKLabelNode!
     var newGameButtonNode: SKSpriteNode!
     var menuButtonNode: SKSpriteNode!
@@ -17,11 +19,68 @@ class GameOver: SKScene {
     var score: Int = 0
     
     override func didMove(to view: SKView) {
+        setupPhisicsWord()
         setupStarField()
         setupScoreNumberLabel()
         setupNewGameButton()
         setupMenuButtonNode()
         saveLeaderboard()
+    }
+    
+    func setupPhisicsWord() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+//        physicsWorld.contactDelegate = self
+        
+        if (weatherKitManager.condition == "Heavy Rain" || weatherKitManager.condition == "Rain"){
+            let background = SKSpriteNode(imageNamed: "Sunny")
+            background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+            addChild(background)
+            background.zPosition = -2
+            background.alpha = 0.5
+        }else if(weatherKitManager.condition == "Blizzard" || weatherKitManager.condition == "Blowing Snow" || weatherKitManager.condition == "Heavy Snow" || weatherKitManager.condition == "Freezing Rain" || weatherKitManager.condition == "Freezing Drizzle"){
+            let background = SKSpriteNode(imageNamed: "Winter")
+            background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+            
+            addChild(background)
+            background.zPosition = -2
+            background.alpha = 1
+        }else{
+            let background = SKSpriteNode(imageNamed: "Sunny")
+            background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+            addChild(background)
+            background.zPosition = -2
+            background.alpha = 1
+        }
+        
+
+    }
+    
+    func setupStarField() {
+        Task{
+            do{
+                try await weatherKitManager.getWeather(latitude:locationManager.latitude, longitude:locationManager.longitude)
+                print("Weather Game Scene: ",weatherKitManager.condition)
+                if (weatherKitManager.condition == "Heavy Rain" || weatherKitManager.condition == "Rain"){
+                    starField = SKEmitterNode(fileNamed: "Rain")
+                    starField.position = CGPoint(x: 0, y: self.frame.maxY)
+                    starField.advanceSimulationTime(10)
+                    addChild(starField)
+                    starField.zPosition = -1
+                }else if(weatherKitManager.condition == "Blizzard" || weatherKitManager.condition == "Blowing Snow" || weatherKitManager.condition == "Heavy Snow" || weatherKitManager.condition == "Freezing Rain" || weatherKitManager.condition == "Freezing Drizzle"){
+                    starField = SKEmitterNode(fileNamed: "Snow")
+                    starField.position = CGPoint(x: 0, y: self.frame.maxY)
+                    starField.advanceSimulationTime(10)
+                    addChild(starField)
+                    starField.zPosition = -1
+                }else{
+                    
+                }
+            }catch{
+                
+            }
+        }
+        
+        
     }
     
     func saveLeaderboard(){
@@ -32,13 +91,13 @@ class GameOver: SKScene {
     
     func setupMenuButtonNode() {
         menuButtonNode = childNode(withName: "menuButton") as? SKSpriteNode
-        menuButtonNode.texture = SKTexture(imageNamed: "home")
+        menuButtonNode.texture = SKTexture(imageNamed: "homeButton")
     }
     
-    func setupStarField() {
-        starField = self.childNode(withName: "starField") as? SKEmitterNode
-        starField.advanceSimulationTime(10)
-    }
+//    func setupStarField() {
+//        starField = self.childNode(withName: "starField") as? SKEmitterNode
+//        starField.advanceSimulationTime(10)
+//    }
     
     func setupScoreNumberLabel() {
         scoreNumberLabel = self.childNode(withName: "scoreNumberLabel") as? SKLabelNode
@@ -47,7 +106,7 @@ class GameOver: SKScene {
     
     func setupNewGameButton() {
         newGameButtonNode = self.childNode(withName: "newGameButton") as? SKSpriteNode
-        newGameButtonNode.texture = SKTexture(imageNamed: "newGameButton")
+        newGameButtonNode.texture = SKTexture(imageNamed: "playAgainLabel")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
--- Up/Scenes/Menu/MenuScene.swift
+++ Up/Scenes/Menu/MenuScene.swift
@@ -18,12 +18,12 @@ class MenuScene: SKScene {
     var newGameButtonNode: SKSpriteNode!
     var difficultyButtonNode: SKSpriteNode!
     var difficultyLabelNode: SKLabelNode!
-    var gameTitleLabelNode: SKLabelNode!
+    var gameTitleLabelNode: SKSpriteNode!
     var tempLabelNode: SKLabelNode!
     var leaderboardButtonNode: SKSpriteNode!
     
     let newGameButtonName = "newGameButton"
-    let difficultyButtonName = "difficultyButton"
+//    let difficultyButtonName = "difficultyButton"
     let leaderboardButtonName = "leaderboardButton"
     
     let userDefaults = UserDefaults.standard
@@ -41,11 +41,12 @@ class MenuScene: SKScene {
     
     override func didMove(to view: SKView) {
         //        setupTempTitleLabel()
+        setupPhisicsWord()
         setupGameTitleLabel()
-        setupStartField()
+//        setupStartField()
         setupNewGameButtonNode()
-        setupDifficultyButtonNode()
-        setupDifficultLabelNode()
+//        setupDifficultyButtonNode()
+//        setupDifficultLabelNode()
         setupLeaderboardButtonNode()
     }
     
@@ -69,10 +70,10 @@ class MenuScene: SKScene {
                     
                 }
             }
-        } else if nodesArray.first?.name == difficultyButtonName {
-            changeDifficulty()
-        } else{
+        } else if nodesArray.first?.name == leaderboardButtonName {
             showingLeaderboard()
+        } else{
+            
         }
         
     }
@@ -84,7 +85,7 @@ class MenuScene: SKScene {
     //    }
     //gameTitleLabel
     private func setupGameTitleLabel() {
-        gameTitleLabelNode = childNode(withName: "gameTitleLabel") as? SKLabelNode
+        gameTitleLabelNode = self.childNode(withName: "gameTitleLabel") as? SKSpriteNode
         let yPosition = gameTitleLabelNode.position.y
         gameTitleLabelNode.position = CGPoint(x: frame.size.width/2, y: yPosition)
     }
@@ -95,50 +96,105 @@ class MenuScene: SKScene {
         newGameButtonNode.position = CGPoint(x: frame.size.width/2, y: yPosition)
     }
     
-    private func setupStartField() {
-        starField = self.childNode(withName: "starField") as? SKEmitterNode
-        starField.advanceSimulationTime(10)
-    }
-    
-    private func setupDifficultyButtonNode() {
-        difficultyButtonNode = self.childNode(withName: difficultyButtonName) as? SKSpriteNode
-        difficultyButtonNode.texture = SKTexture(imageNamed: difficultyButtonName)
-        let yPosition = difficultyButtonNode.position.y
-        difficultyButtonNode.position = CGPoint(x: frame.size.width/2, y: yPosition)
-    }
-    
-    private func setupDifficultLabelNode() {
-        difficultyLabelNode = self.childNode(withName: "difficultyLabel") as? SKLabelNode
-        let yPosition = difficultyLabelNode.position.y
-        difficultyLabelNode.position = CGPoint(x: frame.size.width/2, y: yPosition)
-        if let gameDifficulty = userDefaults.value(forKey: Constants.UserDefaultKeys.GameDifficulty) as? String {
-            difficultyLabelNode.text = gameDifficulty
-        } else {
-            difficultyLabelNode.text = DifficultyOptions.Easy.description
+    func setupPhisicsWord() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+//        physicsWorld.contactDelegate = self
+        
+        if (weatherKitManager.condition == "Heavy Rain" || weatherKitManager.condition == "Rain"){
+            let background = SKSpriteNode(imageNamed: "Sunny")
+            background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+            addChild(background)
+            background.zPosition = -2
+            background.alpha = 0.5
+        }else if(weatherKitManager.condition == "Blizzard" || weatherKitManager.condition == "Blowing Snow" || weatherKitManager.condition == "Heavy Snow" || weatherKitManager.condition == "Freezing Rain" || weatherKitManager.condition == "Freezing Drizzle"){
+            let background = SKSpriteNode(imageNamed: "Winter")
+            background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+            addChild(background)
+            background.zPosition = -2
+            background.alpha = 1
+        }else{
+            let background = SKSpriteNode(imageNamed: "Sunny")
+            background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+            addChild(background)
+            background.zPosition = -2
+            background.alpha = 1
         }
+        
+
     }
+    
+    func setupStarField() {
+        Task{
+            do{
+                try await weatherKitManager.getWeather(latitude:locationManager.latitude, longitude:locationManager.longitude)
+                print("Weather Game Scene: ",weatherKitManager.condition)
+                if (weatherKitManager.condition == "Heavy Rain" || weatherKitManager.condition == "Rain"){
+                    starField = SKEmitterNode(fileNamed: "Rain")
+                    starField.position = CGPoint(x: 0, y: self.frame.maxY)
+                    starField.advanceSimulationTime(10)
+                    addChild(starField)
+                    starField.zPosition = -1
+                }else if(weatherKitManager.condition == "Blizzard" || weatherKitManager.condition == "Blowing Snow" || weatherKitManager.condition == "Heavy Snow" || weatherKitManager.condition == "Freezing Rain" || weatherKitManager.condition == "Freezing Drizzle"){
+                    starField = SKEmitterNode(fileNamed: "Snow")
+                    starField.position = CGPoint(x: 0, y: self.frame.maxY)
+                    starField.advanceSimulationTime(10)
+                    addChild(starField)
+                    starField.zPosition = -1
+                }else{
+                    
+                }
+            }catch{
+                
+            }
+        }
+        
+        
+    }
+    
+//    private func setupStartField() {
+//        starField = self.childNode(withName: "starField") as? SKEmitterNode
+//        starField.advanceSimulationTime(10)
+//    }
+    
+//    private func setupDifficultyButtonNode() {
+//        difficultyButtonNode = self.childNode(withName: difficultyButtonName) as? SKSpriteNode
+//        difficultyButtonNode.texture = SKTexture(imageNamed: difficultyButtonName)
+//        let yPosition = difficultyButtonNode.position.y
+//        difficultyButtonNode.position = CGPoint(x: frame.size.width/2, y: yPosition)
+//    }
+    
+//    private func setupDifficultLabelNode() {
+//        difficultyLabelNode = self.childNode(withName: "difficultyLabel") as? SKLabelNode
+//        let yPosition = difficultyLabelNode.position.y
+//        difficultyLabelNode.position = CGPoint(x: frame.size.width/2, y: yPosition)
+//        if let gameDifficulty = userDefaults.value(forKey: Constants.UserDefaultKeys.GameDifficulty) as? String {
+//            difficultyLabelNode.text = gameDifficulty
+//        } else {
+//            difficultyLabelNode.text = DifficultyOptions.Easy.description
+//        }
+//    }
     
     private func setupLeaderboardButtonNode() {
         leaderboardButtonNode = self.childNode(withName: leaderboardButtonName) as? SKSpriteNode
-        leaderboardButtonNode.texture = SKTexture(imageNamed: difficultyButtonName)
+        leaderboardButtonNode.texture = SKTexture(imageNamed: "leaderboardLabel")
         let yPosition = leaderboardButtonNode.position.y
         leaderboardButtonNode.position = CGPoint(x: frame.size.width/2, y: yPosition)
     }
     
-    func changeDifficulty() {
-        guard let difficulty = DifficultyOptions(rawValue: difficultyLabelNode.text!) else { return }
-        switch difficulty {
-        case .Easy:
-            difficultyLabelNode.text = DifficultyOptions.Medium.description
-            userDefaults.set(DifficultyOptions.Medium.description, forKey: Constants.UserDefaultKeys.GameDifficulty)
-        case .Medium:
-            difficultyLabelNode.text = DifficultyOptions.Hard.description
-            userDefaults.set(DifficultyOptions.Hard.description, forKey: Constants.UserDefaultKeys.GameDifficulty)
-        case .Hard:
-            difficultyLabelNode.text = DifficultyOptions.Easy.description
-            userDefaults.set(DifficultyOptions.Easy.description, forKey: Constants.UserDefaultKeys.GameDifficulty)
-        }
-    }
+//    func changeDifficulty() {
+//        guard let difficulty = DifficultyOptions(rawValue: difficultyLabelNode.text!) else { return }
+//        switch difficulty {
+//        case .Easy:
+//            difficultyLabelNode.text = DifficultyOptions.Medium.description
+//            userDefaults.set(DifficultyOptions.Medium.description, forKey: Constants.UserDefaultKeys.GameDifficulty)
+//        case .Medium:
+//            difficultyLabelNode.text = DifficultyOptions.Hard.description
+//            userDefaults.set(DifficultyOptions.Hard.description, forKey: Constants.UserDefaultKeys.GameDifficulty)
+//        case .Hard:
+//            difficultyLabelNode.text = DifficultyOptions.Easy.description
+//            userDefaults.set(DifficultyOptions.Easy.description, forKey: Constants.UserDefaultKeys.GameDifficulty)
+//        }
+//    }
     
     func showingLeaderboard(){
         GameCenterManager.shared.showLeaderboard()
